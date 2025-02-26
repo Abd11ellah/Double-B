5ced5fe228e0f7ad3f73ddce7020b24482de81a9
MalekOzon <malekozon123@gmail.com>
2025-02-26 19:20:40 +0300

    where work

--- src/components/main/WhatIsUSG.jsx
+++ src/components/main/WhatIsUSG.jsx
@@ -5,7 +5,7 @@ import Frame29 from '../../../public/recourses/Frame 29.png'
 const Forth = () => {
   return (
     <div
-      className=' flex w-full'
+      className=' flex w-full mt-20'
     > 
       {/* photo */}
       <div
@@ -26,22 +26,20 @@ const Forth = () => {
         <div 
           className='mb-[60px] relative'
         >
-          {/* Rectangle */}
-          
           <p  
             className=' font-bold flex justify-center text-3xl  relative z-10 text-[#3A3A3A]'
             >
             What is UGC?
           </p>
+              {/* Rectangle */}
           <div
             className='absolute w-64 h-4 bg-[#FFDF40] top-5 right-[262px] z-0 '
-          >
-          </div>
+          />
+          
         </div>
         <p
           className='ml-[48px] font-bold w-[739px] h-[136px] text-2xl text-[#3A3A3A]'
         >
-
         User-Generated Content (UGC) videos are created and
         produced by the audience or customers, rather than by 
         companies or professionals. These videos are often 
