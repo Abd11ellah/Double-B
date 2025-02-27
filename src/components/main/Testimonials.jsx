2c31caeca16a6f0c409da733d318655ccc58f8a1
Abd11ellah <abdulilahalhamwi@gmail.com>
2025-02-27 11:16:41 +0300

    some fixes

--- src/components/main/Testimonials.jsx
+++ src/components/main/Testimonials.jsx
@@ -6,7 +6,13 @@ import frame35 from "../../../public/recourses/Rectangle 35.png"
 import linkedin from "../../../public/recourses/Rectangle 39.png"
 import bih from "../../../public/recourses/Group.png"
 import x from "../../../public/recourses/Rectangle 36.png"
-
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
 const Testimonials = () => {
   return (
     <div className='my-16  '>
@@ -37,26 +43,41 @@ const Testimonials = () => {
       </div>
 
       {/* Content */}
-      <div className='mt-8 flex justify-center  '>
-        <div className='flex justify-center  border-solid border-[#C4C4C4] rounded-xl font-bold border-2 p-8'>
-          <Image src={frame35} className='w-[230px] h-[224px] mr-7' />
-          <div className=' mt-6'>
-            <h3>
-            Mohammad Shaheen
-            </h3>
-            <p className='mt-6 text-[#3A3A3A] w-[600px] font-bold text-base'>
-            I enjoy creating videos for brands throughout my career in content creation.
-            During this journey, I have been very happy with my ratings and achieving 
-            over 100 videos for brands across various cities in Syria.
-            </p>
-            <div className='mt-6 flex '>
-              <Image src={linkedin} className='w-[23px] h-[23px] mr-2' />
-              <Image src={x} className='w-[23px] h-[23px] mr-2' />
-              <Image src={bih} className='w-[23px] h-[23px]' />
+      <Carousel className="w-[65%] mx-auto " 
+                opts={{
+                loop: true,
+              }}
+      >
+      <CarouselContent>
+        {Array.from({ length: 5 }).map((_, index) => (
+          <CarouselItem key={index}>
+            <div className='mt-8 flex justify-center  '>
+              <div className='flex justify-center  border-solid border-[#C4C4C4] rounded-xl font-bold border-2 p-8'>
+                <Image src={frame35} alt="Example of user-generated content" className='w-[230px] h-[224px] mr-7' />
+                <div className=' mt-6'>
+                  <h3>
+                  Mohammad Shaheen
+                  </h3>
+                  <p className='mt-6 text-[#3A3A3A] w-[600px] font-bold text-base'>
+                  I enjoy creating videos for brands throughout my career in content creation.
+                  During this journey, I have been very happy with my ratings and achieving 
+                  over 100 videos for brands across various cities in Syria.
+                  </p>
+                  <div className='mt-6 flex '>
+                    <Image src={linkedin} alt="Example of user-generated content" className='w-[23px] h-[23px] mr-2' />
+                    <Image src={x} alt="Example of user-generated content" className='w-[23px] h-[23px] mr-2' />
+                    <Image src={bih} alt="Example of user-generated content" className='w-[23px] h-[23px]' />
+                  </div>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-      </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+     
     </div>
   )
 }
